0fe73c24401eb1dabe0b7a3094cadcc18ea6941d
achref-aouissaoui <medachrefaouissaoui@gmail.com>
2020-07-14 00:13:35 +0100

    final

--- project/main.js
+++ project/main.js
@@ -1,7 +1,70 @@
 
 $(document).ready(function(){
-
+$("#page2").hide();
+$("#page3").hide();
+$("#page4").hide();
+$("#page5").hide();
 	var count=0;
+$(document).ready(function(){
+
+  $("#answer1-1"||"#answer1-2").click(function(){
+    $("#page1").hide();
+     $("#page2").show();
+  });
+
+  $("#answer1-2").click(function(){
+    $("#page1").hide();
+    $("#page2").show();
+  });
+  
+   $("#answer2-1").click(function(){
+    $("#page2").hide();
+    $("#page3").show();
+  });
+
+   $("#answer2-2").click(function(){
+    $("#page2").hide();
+     $("#page3").show();
+  });
+
+   $("#answer2-3").click(function(){
+    $("#page2").hide();
+     $("#page3").show();
+  });
+
+   $("#answer2-4").click(function(){
+    $("#page2").hide();
+     $("#page3").show();
+  });
+   $("#answer3-1").click(function(){
+    $("#page3").hide();
+     $("#page4").show();
+  });
+   $("#answer3-2").click(function(){
+    $("#page3").hide();
+    $("#page4").show();
+  });
+   $("#answer3-3").click(function(){
+    $("#page3").hide();
+    $("#page4").show();
+  });
+  $("#answer3-4").click(function(){
+    $("#page3").hide();
+    $("#page4").show();
+  });
+   $("#answer4-1").click(function(){
+    $("#page4").hide();
+    $("#page5").show();
+  });
+  $("#answer4-2").click(function(){
+    $("#page4").hide();
+    $("#page5").show();
+  });
+});
+
+
+
+
 
 		$('#answer1-1').click(function(){
 			count++
